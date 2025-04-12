72a84dd2d05ea458483175adf40f263ef346b284
Ari0115 <arihumvarper@gmail.com>
2025-04-12 02:21:02 +0200

    changed movement to SDL_EVEN with switch-statments instead of SDL_SCANCODE_KEY with if-statements

--- client/src/main.c
+++ client/src/main.c
@@ -21,7 +21,7 @@ int map[MAP_HEIGHT][MAP_WIDTH] = {
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 };
 
-int main(void) {
+int main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         SDL_Log("SDL_Init Error: %s", SDL_GetError());
         return -1;
@@ -33,91 +33,87 @@ int main(void) {
         return -1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("Cute Bunny", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                                          SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    SDL_Window* window = SDL_CreateWindow("Cute Bunny",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+
     if (!window) {
-        SDL_Log("Failed to create window: %s", SDL_GetError());
-        IMG_Quit();
-        SDL_Quit();
-        return -1;
+        SDL_Log("Window creation failed: %s", SDL_GetError());
+        IMG_Quit(); SDL_Quit(); return -1;
     }
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
-        SDL_Log("Failed to create renderer: %s", SDL_GetError());
-        SDL_DestroyWindow(window);
-        IMG_Quit();
-        SDL_Quit();
-        return -1;
+        SDL_Log("Renderer creation failed: %s", SDL_GetError());
+        SDL_DestroyWindow(window); IMG_Quit(); SDL_Quit(); return -1;
     }
 
-    SDL_Texture* tileTextures[2];
-    tileTextures[0] = IMG_LoadTexture(renderer, "assets/grass.png");
-    tileTextures[1] = IMG_LoadTexture(renderer, "assets/water.png");
+    SDL_Texture* tileTextures[2] = {
+        IMG_LoadTexture(renderer, "assets/grass.png"),
+        IMG_LoadTexture(renderer, "assets/water.png")
+    };
 
     if (!tileTextures[0] || !tileTextures[1]) {
-        SDL_Log("Failed to load tiles: %s", SDL_GetError());
-        for (int i = 0; i < 2; ++i) if (tileTextures[i]) SDL_DestroyTexture(tileTextures[i]);
-
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        IMG_Quit();
-        SDL_Quit();
-        return -1;
+        SDL_Log("Failed to load tile textures.");
+        for (int i = 0; i < 2; i++) if (tileTextures[i]) SDL_DestroyTexture(tileTextures[i]);
+        SDL_DestroyRenderer(renderer); SDL_DestroyWindow(window);
+        IMG_Quit(); SDL_Quit(); return -1;
     }
 
     SDL_Texture* bunnyWalkRight = IMG_LoadTexture(renderer, "assets/panda_spritesheets/panda_walk_right.png");
-    SDL_Texture* bunnyWalkLeft = IMG_LoadTexture(renderer, "assets/panda_spritesheets/panda_walk_left.png");
+    SDL_Texture* bunnyWalkLeft  = IMG_LoadTexture(renderer, "assets/panda_spritesheets/panda_walk_left.png");
 
     if (!bunnyWalkRight || !bunnyWalkLeft) {
-        SDL_Log("Could not load spritesheets: %s", SDL_GetError());
-        for (int i = 0; i < 2; ++i) SDL_DestroyTexture(tileTextures[i]);
-
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        IMG_Quit();
-        SDL_Quit();
-        return -1;
+        SDL_Log("Failed to load sprite sheets.");
+        for (int i = 0; i < 2; i++) SDL_DestroyTexture(tileTextures[i]);
+        SDL_DestroyRenderer(renderer); SDL_DestroyWindow(window);
+        IMG_Quit(); SDL_Quit(); return -1;
     }
-
-    int frame = 0;
-    Uint32 lastFrameTime = SDL_GetTicks();
 
     int playerX = 100, playerY = 100;
     int speed = 10;
     bool facingLeft = false;
-
-    SDL_Event event;
+    int frame = 0;
+    Uint32 lastFrameTime = SDL_GetTicks();
     bool isRunning = true;
 
+    bool movingUp = false, movingDown = false;
+    bool movingLeft = false, movingRight = false;
+
+    SDL_Event event;
+
     while (isRunning) {
-        while (SDL_PollEvent(&event)) if (event.type == SDL_QUIT) isRunning = false;
-
-        const Uint8* keys = SDL_GetKeyboardState(NULL);
-        int prevX = playerX;
-        int prevY = playerY;
-        bool isMoving = false;
-
-        int moveX = 0;
-        int moveY = 0;
-
-        if (keys[SDL_SCANCODE_W]) {
-            moveY -= speed;
-            isMoving = true;
-        } else if (keys[SDL_SCANCODE_S]) {
-            moveY += speed;
-            isMoving = true;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT: isRunning = false; break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym) {
+                        case SDLK_w: movingUp = true; break;
+                        case SDLK_s: movingDown = true; break;
+                        case SDLK_a: movingLeft = true; facingLeft = true; break;
+                        case SDLK_d: movingRight = true; facingLeft = false; break;
+                    }
+                    break;
+                case SDL_KEYUP:
+                    switch (event.key.keysym.sym) {
+                        case SDLK_w: movingUp = false; break;
+                        case SDLK_s: movingDown = false; break;
+                        case SDLK_a: movingLeft = false; break;
+                        case SDLK_d: movingRight = false; break;
+                    }
+                    break;
+            }
         }
 
-        if (keys[SDL_SCANCODE_A]) {
-            moveX -= speed;
-            facingLeft = true;
-            isMoving = true;
-        } else if (keys[SDL_SCANCODE_D]) {
-            moveX += speed;
-            facingLeft = false;
-            isMoving = true;
-        }
+        int prevX = playerX, prevY = playerY;
+        int moveX = 0, moveY = 0;
+
+        if (movingUp)    moveY -= speed;
+        if (movingDown)  moveY += speed;
+        if (movingLeft)  moveX -= speed;
+        if (movingRight) moveX += speed;
+
+        bool isMoving = moveX != 0 || moveY != 0;
 
         playerX += moveX;
         playerY += moveY;
@@ -125,6 +121,7 @@ int main(void) {
         int tileX = playerX / TILE_SIZE;
         int tileY = playerY / TILE_SIZE;
 
+        // Basic collision detection
         if (tileX < 0 || tileX >= MAP_WIDTH || tileY < 0 || tileY >= MAP_HEIGHT || map[tileY][tileX] == 1) {
             playerX = prevX;
             playerY = prevY;
@@ -139,30 +136,27 @@ int main(void) {
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        for (int y = 0; y < MAP_HEIGHT; ++y) {
-            for (int x = 0; x < MAP_WIDTH; ++x) {
+        for (int y = 0; y < MAP_HEIGHT; y++) {
+            for (int x = 0; x < MAP_WIDTH; x++) {
                 SDL_Rect dst = { x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE };
                 SDL_RenderCopy(renderer, tileTextures[map[y][x]], NULL, &dst);
             }
         }
 
-        SDL_Rect srcRect = { frame * SPRITE_WIDTH, 0, SPRITE_WIDTH, SPRITE_HEIGHT };
+        SDL_Rect srcRect  = { frame * SPRITE_WIDTH, 0, SPRITE_WIDTH, SPRITE_HEIGHT };
         SDL_Rect destRect = { playerX, playerY, SPRITE_WIDTH, SPRITE_HEIGHT };
-
         SDL_RenderCopy(renderer, facingLeft ? bunnyWalkLeft : bunnyWalkRight, &srcRect, &destRect);
-        SDL_RenderPresent(renderer);
 
+        SDL_RenderPresent(renderer);
         SDL_Delay(16);
     }
 
     for (int i = 0; i < 2; ++i) SDL_DestroyTexture(tileTextures[i]);
-
     SDL_DestroyTexture(bunnyWalkRight);
     SDL_DestroyTexture(bunnyWalkLeft);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     IMG_Quit();
     SDL_Quit();
-
     return 0;
 }
