4c9c99e50515fdb1584b406541a7ce1d16caf8c3
Ari0115 <arihumvarper@gmail.com>
2025-04-29 02:31:46 +0200

    .

--- lib/src/Bullet.c
+++ lib/src/Bullet.c
@@ -3,17 +3,7 @@
 #include <math.h>
 
 Bullet* createBullet(SDL_Renderer* renderer, float startX, float startY, float dirX, float dirY, int whoShot) {
-Bullet* createBullet(SDL_Renderer* renderer, float startX, float startY, float dirX, float dirY, int whoShot) {
     Bullet* bullet = (Bullet*)malloc(sizeof(Bullet));
-    if (!bullet) return NULL;
-
-    float length = sqrtf(dirX * dirX + dirY * dirY);
-    if (length > 0.0f) {
-        bullet->dx = (dirX / length) * BULLET_SPEED;
-        bullet->dy = (dirY / length) * BULLET_SPEED;
-    } else {
-        bullet->dx = BULLET_SPEED;
-        bullet->dy = 0;
     if (!bullet) return NULL;
 
     float length = sqrtf(dirX * dirX + dirY * dirY);
@@ -28,32 +18,6 @@ Bullet* createBullet(SDL_Renderer* renderer, float startX, float startY, float d
     bullet->x = startX;
     bullet->y = startY;
     bullet->whoShot = whoShot;
-    bullet->bornTime = SDL_GetTicks();
-
-    bullet->texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 8, 8);
-    if (bullet->texture) {
-        SDL_SetRenderTarget(renderer, bullet->texture);
-        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-        SDL_RenderClear(renderer);
-        SDL_SetRenderTarget(renderer, NULL);
-    }
-
-    return bullet;
-}
-
-void moveBullet(Bullet* bullet) {
-    bullet->x += bullet->dx;
-    bullet->y += bullet->dy;
-}
-
-void drawBullet(Bullet* bullet, SDL_Renderer* renderer) {
-    SDL_Rect rect = { (int)bullet->x, (int)bullet->y, 8, 8 };
-    SDL_RenderCopy(renderer, bullet->texture, NULL, &rect);
-}
-
-void destroyBullet(Bullet* bullet) {
-    if (bullet) {
-        if (bullet->texture) SDL_DestroyTexture(bullet->texture);
     bullet->bornTime = SDL_GetTicks();
 
     bullet->texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 8, 8);
