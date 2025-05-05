e7a26469839fe5a28d4cfbef3c1270377ba6ae42
Ari0115 <arihumvarper@gmail.com>
2025-05-05 17:24:10 +0200

    remv []

--- lib/include/Network.h
+++ lib/include/Network.h
@@ -4,9 +4,9 @@
 #define MAX_ANIMALS 6
 #define SERVER_PORT 2000
 
-typedef enum { READY, UP, DOWN, LEFT, RIGHT, FIRE, BLOCKED, CONNECTING } ClientCommand;
-
 typedef enum { MENU, ONGOING  } GameState;
+typedef struct { float x, y, dx, dy; } BulletData;
+typedef enum { READY, UP, DOWN, LEFT, RIGHT, FIRE, BLOCKED, CONNECTING } ClientCommand;
 
 typedef struct {
     int health, type;
@@ -16,17 +16,15 @@ typedef struct {
 typedef struct {
     Animal animals;
     ClientCommand command[7];
-    int playerNumber, slotsTaken[6], numberOfBullets;
+    int playerNumber, slotsTaken[MAX_ANIMALS], numberOfBullets;
     float bulletDx, bulletDy, bulletStartX, bulletStartY;
 } ClientData;
-
-typedef struct { float x, y, dx, dy; } BulletData;
 
 typedef struct {
     GameState gameState;
     Animal animals[MAX_ANIMALS];
     int fire, whoShot, slotsTaken[MAX_ANIMALS];
-    int assignedID, numberOfBullets, numberOfPlayers;
+    int numberOfBullets, numberOfPlayers;
     float bulletDx, bulletDy, bulletStartX, bulletStartY;;
 } ServerData;
 
