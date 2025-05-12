179d2810007aab94cdb6af4cd509f4802b467420
Majdmkl <majd.makhoul.1104@gmail.com>
2025-05-12 18:58:21 +0200

    map.h new

--- lib/include/Map.h
+++ lib/include/Map.h
@@ -6,30 +6,30 @@
 
 #define SCREEN_WIDTH 1920
 #define SCREEN_HEIGHT 1024
-#define MAX_WALLS 6
-//! same here
+#define MAX_WALLS 100
+
 #define NR_X 16
 #define NR_Y 30
 #define TILE_SIZE 64
-//! same here
+
 
 typedef struct MAP{
     int x_min, x_max;
     int y_min, y_max;
     int width, height;
-    //! when real map is used remove this
     SDL_Texture** tileTextures;
     SDL_Texture* treeTexture;
     SDL_Texture* cottageTexture;
-    //! when real map is used remove this
 } MAP;
 
 extern MAP walls[MAX_WALLS];
-//! 👇
+
+void loadMap(SDL_Renderer *renderer);
+void drawMap(SDL_Renderer *renderer);
 void destroyMap(MAP* map);
 MAP* createMap(SDL_Renderer* renderer);
 void renderMap(MAP* map, SDL_Renderer* renderer);
-//! same for 👆
+
 void convertWallsToRects(MAP *walls, SDL_Rect *rects, int count);
 
 #endif
